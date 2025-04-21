f415d009be8d4c3a7ff7ff8e1a8b78b1e0967f47
Jessica H <jessicahebert75@gmail.com>
2025-04-21 11:29:40 -0700

    Added utillity for ordering balances

--- src/PayoffApp.java
+++ src/PayoffApp.java
@@ -1,8 +1,13 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class PayoffApp {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        List<Double> balances = new ArrayList<Double>();
 
         while(scan.hasNextLine()) {
             String name = scan.nextLine();
@@ -11,11 +16,17 @@ public class PayoffApp {
             double balance = scan.nextDouble();
 
             // Consume \n after balance input 
+        
             if(scan.hasNextLine()) scan.nextLine();
+
+            balances.add(balance);
 
             String aprString = String.format("%.2f%%", apr);
             String balanceString = String.format("$%.2f", balance);
             System.out.println(name + ": " + "APR: " + aprString + " Balance: " + balanceString);
         }
+        System.out.println(balances);
+        Collections.sort(balances, Collections.reverseOrder());
+        System.out.println(balances);
     }
 }
