220c1fb35cb447ced9d406bdf4b8b5676cd6bad7
Jessica H <jessicahebert75@gmail.com>
2025-04-21 11:58:48 -0700

    made changes to payoff

--- src/PayoffApp.java
+++ src/PayoffApp.java
@@ -1,32 +1,13 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
 
 public class PayoffApp {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
 
-        List<Double> balances = new ArrayList<Double>();
-
-        while(scan.hasNextLine()) {
-            String name = scan.nextLine();
-
-            double apr = scan.nextDouble();
-            double balance = scan.nextDouble();
-
-            // Consume \n after balance input 
-        
-            if(scan.hasNextLine()) scan.nextLine();
-
-            balances.add(balance);
-
-            String aprString = String.format("%.2f%%", apr);
-            String balanceString = String.format("$%.2f", balance);
-            System.out.println(name + ": " + "APR: " + aprString + " Balance: " + balanceString);
-        }
-        System.out.println(balances);
-        Collections.sort(balances, Collections.reverseOrder());
-        System.out.println(balances);
+        //     String aprString = String.format("%.2f%%", apr);
+        //     String balanceString = String.format("$%.2f", balance);
+        //     System.out.println(name + ": " + "APR: " + aprString + " Balance: " + balanceString);
+        // }
+        // System.out.println(balances);
+        // Collections.sort(balances, Collections.reverseOrder());
+        // System.out.println(balances);
     }
 }
