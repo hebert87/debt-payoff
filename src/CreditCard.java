b0a2ba26a8a1d36879f6babe48ae2a4d56a0e29e
Jessica H <jessicahebert75@gmail.com>
2025-04-23 11:28:42 -0700

    finished the pay off app in class

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -1,3 +1,50 @@
-public class CreditCard {
+
+
+public class CreditCard implements Comparable<CreditCard> {
+    private String name;
+    private double apr;
+    private double balance;
+
+    public CreditCard(String name,double apr,double balance) {
+        this.name = name;
+        this.apr = apr;
+        this.balance = balance;
+    }
+
+    public double gitApr(){
+        return apr;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getApr() {
+        return apr;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    @Override
+    public String toString(){
+        return name + " " + balance;
+    }
+    public double monthlyIntrest(){
+        return balance *apr/12/100;
+    }
+    public int compareTo(CreditCard other){
+    
+
+        if(monthlyIntrest() > other.monthlyIntrest()){
+            return  1;
+        }
+        if(monthlyIntrest() < other.monthlyIntrest()){
+            return -1;
+        }
+        return 0;
+        
+    }
     
 }
--- src/PayoffApp.java
+++ src/PayoffApp.java
@@ -1,13 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
 public class PayoffApp {
     public static void main(String[] args) {
+        CreditCard amex = new CreditCard("American Express",22,1007);
+        CreditCard discover = new CreditCard("Discover",33,500);
+        System.out.println(amex.monthlyIntrest());
+        Scanner scan = new Scanner(System.in);
 
-        //     String aprString = String.format("%.2f%%", apr);
-        //     String balanceString = String.format("$%.2f", balance);
-        //     System.out.println(name + ": " + "APR: " + aprString + " Balance: " + balanceString);
-        // }
-        // System.out.println(balances);
-        // Collections.sort(balances, Collections.reverseOrder());
-        // System.out.println(balances);
-    }
+        List<CreditCard> cards = new ArrayList<>();
+        
+
+        while(scan.hasNextLine()) {
+            String name = scan.nextLine();
+
+            double apr = scan.nextDouble();
+            double balance = scan.nextDouble();
+
+            // Consume \n after balance input 
+        
+            if(scan.hasNextLine()) scan.nextLine();
+
+           
+
+            String aprString = String.format("%.2f%%", apr);
+            String balanceString = String.format("$%.2f", balance);
+            System.out.println(name + ": " + "APR: " + aprString + " Balance: " + balanceString);
+        }
+        System.out.println(cards);
+        Collections.sort(cards, Collections.reverseOrder());
+        System.out.println(cards);
+     }
 }
